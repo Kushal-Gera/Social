81c4af9ba47cf606ec4aa07b60857951c00600b4
Kushal Gera <kushalgera1212@gmail.com>
2021-01-28 00:31:01 +0530

    Splashscreen, BottomNavBar completed

--- app/src/main/java/kushal/application/social/MainActivity.java
+++ app/src/main/java/kushal/application/social/MainActivity.java
@@ -1,8 +1,8 @@
 package kushal.application.social;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/kushal/application/social/Splashscreen.java
@@ -0,0 +1,24 @@
+package kushal.application.social;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 800);
+
+
+    }
+}
